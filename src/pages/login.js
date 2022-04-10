519d6d6374d26bb1a933fea45127fc65422f665f
JoabPa <joabparedes.ec@gmail.com>
2022-04-10 01:21:28 -0400

    working on the Login page

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useContext, useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import FirebaseContext from '../context/firebase';
 
 const Login = (props) => {
-  return <p>I am the Login page!</p>;
+  const navigate = useNavigate();
+  const { firebase } = useContext(FirebaseContext);
+
+  const [emailAddress, setEmailAddress] = useState('');
+  const [password, setPassword] = useState('');
+
+  const [error, setError] = useState('');
+  const isInvalid = password === '' || emailAddress === '';
+
+  const handleLogin = (event) => {};
+
+  useEffect(() => {
+    document.title = 'Login - Instagram';
+  }, []);
+
+  return (
+    <div className='container flex mx-auto max-w-screen-md items-center h-screen'>
+      <p>I have no idea</p>
+    </div>
+  );
 };
 
 export default Login;
