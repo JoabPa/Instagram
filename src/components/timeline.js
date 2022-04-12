1887ae3074ead1afb96213c78075675f6df1a928
JoabPa <joabparedes.ec@gmail.com>
2022-04-12 00:07:17 -0400

    working on the sidebar/user.js component

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -1,5 +1,9 @@
 const Timeline = () => {
-  return <p>I am the Timeline</p>;
+  return (
+    <div className='container col-span-2'>
+      <p>I am the Timeline</p>
+    </div>
+  );
 };
 
 export default Timeline;
