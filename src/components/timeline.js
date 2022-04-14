8aff40ee9e098f53d70a4cc3a0aa9687a8834c73
JoabPa <joabparedes.ec@gmail.com>
2022-04-14 00:51:32 -0400

    getting the photos from followed users with use-photos hook

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -1,4 +1,9 @@
+import Skeleton from "react-loading-skeleton";
+import usePhotos from "../hooks/use-photos";
+
 const Timeline = () => {
+  const { photos } = usePhotos();
+
   return (
     <div className='container col-span-2'>
       <p>I am the Timeline</p>
