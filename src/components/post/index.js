fc7c94df0960525615ce833900043de19eba1932
JoabPa <joabparedes.ec@gmail.com>
2022-04-14 22:42:33 -0400

    displaying the comments in the post

--- src/components/post/index.js
+++ src/components/post/index.js
@@ -4,6 +4,7 @@ import Header from './header';
 import Image from './image';
 import Actions from './actions';
 import Footer from './footer';
+import Comments from './comments';
 
 const Post = ({ content }) => {
   const commentInput = useRef(null);
@@ -20,6 +21,12 @@ const Post = ({ content }) => {
         handleFocus={handleFocus}
       />
       <Footer caption={content.caption} username={content.username} />
+      <Comments
+        docId={content.docId}
+        comments={content.comments}
+        posted={content.dateCreated}
+        commentInput={commentInput}
+      />
     </div>
   );
 };
